657f501c34ec7cc058e68ec82cdbf96b9c80bfff
Kevin Balmores <balmoresk3v1n@gmail.com>
2023-03-20 21:01:13 +0800

    implemented some Weather API

---
+++ js/script.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const https = require('https')
+const app = express();
+
+app.listen(3000, function () {  
+    console.log('server is running on port 3000...');
+});
+
+// app.use(express.static('public'));
+
+
+// API CALL: https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// API KEY:  d871f7208e65caf916aac8be8dce0be6
+app.get('/', (req, res)=>{
+    const url = 'https://api.openweathermap.org/data/2.5/weather?q=CALAPAN&appid=d871f7208e65caf916aac8be8dce0be6&units=metric';
+
+    https.get(url, function(response){
+        console.log(response.statusCode);
+        // console.log(response.headers);
+
+        response.on('data', (d) => {
+            const weatherData = JSON.parse(d);
+
+            console.log(weatherData);
+
+            console.log("City: " + weatherData.name + ', ' + weatherData.sys.country);
+            console.log('Temp: ' + weatherData.main.temp + ' Celsius');
+            console.log('Weather: ' + weatherData.weather[0].description);
+        })
+    });
+
+    res.send("server is up and running")
+})
+
+
